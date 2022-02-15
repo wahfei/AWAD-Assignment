58327dcad7db726dc5f75da63050e9e9656f5118
wahfei <wahfeilee@yahoo.com>
2022-02-15 16:39:55 +0800

    added files

---
+++ resources/views/Login.blade.php
@@ -0,0 +1,15 @@
+<x-menu/>
+<html>
+    <head>
+        <link rel="stylesheet"  type="text/css" href="/resources/css/globalStyle.css">
+    </head>
+    <body>
+        <div class="login">
+            <h2>Login</h2>
+        </div>
+    </body>
+    <footer>
+
+    </footer>
+
+</html>
---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,33 @@
+<div>
+    <ul>
+        <li><a href="home">HOME</a></li>
+        <li><a href="#">MEN</a></li>
+        <li><a href="#">WOMEN</a></li>
+        <LI><a href="#">KIDS</a></LI>
+        <li><a href="#">SHOPPING CART</a></li>
+    </ul>
+</div>
+<style>
+ul{
+    display: flex;
+    justify-content: center;
+}
+li {
+    list-style-type: none;
+    letter-spacing: 1px;
+    font-size: 13px;
+}
+a {
+    text-decoration: none;
+    padding: 20px 30px;
+    display: flex;
+}
+a:hover{
+    background-color: gray;
+    transition: .5s;
+}
+body{
+    margin: 0;
+    padding: 0;
+}
+</style>
